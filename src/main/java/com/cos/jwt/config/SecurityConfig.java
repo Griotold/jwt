8c6d1afab4770b5a33ca4e2823ec2c58c3ec19a3
Griotold <gotjd9773@naver.com>
2023-04-20 12:18:59 +0900

    jwt를 위한 security 설정

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,28 +1,43 @@
 package com.cos.jwt.config;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CorsFilter corsFilter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // JWT 기본 사용법
+        // csrf, STATELESS, formLogin, httpBasic은 모두 disable 처리
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
+                .addFilter(corsFilter)
+                .formLogin().disable()
+                .httpBasic().disable()
                 .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
                 .antMatchers(
-                        "/h2-console/**"    // 여기!
-                ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .csrf().disable()  // CSRF 보호 해제
-                .headers().frameOptions().disable();  // X-Frame-Options 보호 해제
+                        "/h2-console/**"
+                ).permitAll();
 
     }
 }
